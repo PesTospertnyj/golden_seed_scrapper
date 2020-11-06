5e8991184bd4c9b35cfd326cde622fc3d261c0dc
PesTospertnyj <artur@lineup.com.ua>
2020-11-06 19:46:40 +0200

    remove config.js

--- config/config.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-    PG_USER: 'postgres',
-    PG_PASSWORD: 'root',
-    PG_HOST: 'localhost',
-    PG_DATABASE: 'golden_seed',
-    PG_PORT: '5432',
-    SCRAPPING_URL: 'https://cbddosagecalculator.com/start/'
-}
